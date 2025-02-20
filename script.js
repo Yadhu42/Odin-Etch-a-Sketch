f9894a7237c17c34d2a81c000646786b339faa7a
yadhuks42 <yadhuks42@gmail.com>
2025-02-20 07:49:29 +0000

    Odin Extra Credit - implement progressive darkening effect, each interaction increases opacity by 10%(up to 10 times);

--- script.js
+++ script.js
@@ -2,25 +2,52 @@ const container = document.querySelector(".container");
 
 function squares(num){
 
-    let gap = (num-1)*5;
+    //The flex container is a box of side 650px with a gap of 5px between each square inside
+    //The side of square is determined taking the gap into consideration while trying to maintain the container size
+
+    let gap = (num+1)*5;
     let length = (650-gap)/num;
     let sqs = Math.round(length * 100)/100;
 
     for(i=1;i<=num*num;i++){
         let sq = document.createElement("div");
         sq.setAttribute("class","sq");
-        sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color: #C8A2C8; cursor: pointer`);
+
+    //creating a hidden p inside each square to keep count of mouseover to determine opacity
+
+        const p = document.createElement("p");         
+        sq.appendChild(p);
+        p.textContent=`${1}`;
+        p.setAttribute("style","visibility: hidden;");
+        sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color: rgb(200, 162, 200,0.15); cursor: pointer`);
         sq.addEventListener("mouseover",()=>{
-            sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color:rgb(0, 0, 0); cursor: pointer`);
+
+    //once counter reaches 10, maximum opacity is achieved
+
+            if(p.textContent<=10){                 
+                let count = p.textContent;
+                p.textContent++;
+                sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color:rgb(109, 85, 109,${(0.15+(count/10))}); cursor: pointer`);
+                //console.log(count); 
+            }
+            else{
+                sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color:rgb(109, 85, 109); cursor: pointer`);
+            }
+            
         });
         sq.addEventListener("dblclick",()=>{
-            sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color:#C8A2C8; cursor: pointer`);
+            p.textContent=`${1}`;
+            count = p.textContent;
+            sq.setAttribute("style",`flex-basis:${sqs}px; height:${sqs}px; flex-shrink: 0; background-color:rgb(109, 85, 109,0.15); cursor: pointer`);
+            
         });
         container.appendChild(sq);
     }
+
+
 }
 
-let v = squares(30);
+let v = squares(16);
 
 
 
