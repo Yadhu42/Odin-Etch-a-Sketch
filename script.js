88fa8c780d3eb7be4bc70f18f10ff337fcc04a3e
yadhuks42 <yadhuks42@gmail.com>
2025-02-25 14:01:19 +0000

    Use functions for creating grid, changing modes and resetting

--- script.js
+++ script.js
@@ -2,124 +2,55 @@ const container = document.querySelector(`.container`);
 const main = document.querySelector(`.main`);
 const size = document.querySelector(`#size`);
 const reset = document.querySelector(`#clear`);
-const randomize = document.querySelector(`#randomize`);
-let rainbowMode = false;
+const mode = document.querySelector(`#dmode`);
 
-randomize.addEventListener("input",(event) =>{
-    rainbowMode=true;
-    console.log("rainbow mode on");
+document.addEventListener(`DOMContentLoaded`,()=>{
+let del = squares(13);
+mode.value=`Drawing`;    
+remover(`Drawing`);
+
+size.addEventListener(`click`,()=>{
+    let input = prompt(`Enter grid size (Max 60)`);
+    if(input){
+        del.forEach((element) =>{
+            element.remove();
+        });
+        del = squares(input);
+    }
 });
 
+mode.addEventListener(`input`,(event)=>{
+    remover(event.target.value);
+});
+
+reset.addEventListener(`click`,()=>{
+    del.forEach((element) =>{
+        element.style.backgroundColor=`#ffff`
+        element.setAttribute(`counter`,`1`);
+    });
+});
+
+});
 
 function squares(num){
-    //The flex container is a box of side 650px with a gap of 5px between each square inside
-    //The side of square is determined taking the gap into consideration while trying to maintain the container size
-    randomize.checked=false;
+    container.style.border = `1px solid black`;
     let containerLength= 625;
-    let squareSide = Math.round(((containerLength/num)* 100))/100;
-    let squareBorder = 1.6*num;
-    container.style.width=`${625+squareBorder}px`;
-    container.style.height=`${625+squareBorder}px`;
+    let squareSide = containerLength/num;
+    container.style.width=`${containerLength+1.8}px`;
     const defaultColor = `#ffff`;
     
 
     for(i=1;i<=num*num;i++){
         let sq = document.createElement(`div`);
         sq.setAttribute(`class`,`sq`);
+        sq.setAttribute(`counter`,`1`);
 
-    //creating a hidden p inside each square to keep count of mouseover to determine opacity
-
-        const p = document.createElement(`p`);         
-        sq.appendChild(p);
-        p.textContent=`${1}`;
-
-        sq.style.flexBasis=`${squareSide}px`;
+        sq.style.margin=`0`;
+        sq.style.padding=`0`;
+        sq.style.width=`${squareSide}px`;
         sq.style.height=`${squareSide}px`;
-        sq.style.border=`solid 1px black`;
+        sq.style.border=`1px solid lightgray`;
         sq.style.backgroundColor = defaultColor;
-        
-        //sq.addEventListener(`mouseover`,()=>{
-    //once counter reaches 10, maximum opacity is achieved
-
-        //     if(p.textContent<=10){                 
-        //         let count = p.textContent;
-        //         p.textContent++;
-        //         sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(200, 162, 200,${(10+(count*10))}%)`);
-        //         //console.log(count); 
-        //     }
-        //     else{
-        //         sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(200, 162, 200);`);
-        //     }
-            
-        // });    
-        // reset.addEventListener(`click`,() => {
-        //     p.textContent=`${1}`;
-        //     count = p.textContent;
-        //     sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color: rgb(200, 162, 200,0.15);`);
-        // });
-
-        // randomize.addEventListener(`click`,() =>{
-        //     if(randomize.checked){
-        //         p.textContent=`${1}`;
-        //         count = p.textContent;
-        //         let r = Math.round((Math.random()*256)+1);
-        //         let g = Math.round((Math.random()*256)+1);
-        //         let b = Math.round((Math.random()*256)+1);
-        //         //console.log(r,g,b);
-
-        //         sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color: rgb(${r}, ${g}, ${b},10%); `);
-        //         sq.addEventListener(`mouseover`,()=>{
-
-        //         //once counter reaches 10, maximum opacity is achieved
-
-        //             if(p.textContent<=10){                 
-        //                 let count = p.textContent;
-        //                 p.textContent++;
-        //                 sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(${r}, ${g}, ${b},${(10+(count*10))}%);`);
-        //                 //console.log(count); 
-        //             }
-        //             else{
-        //                 r = Math.round((Math.random()*256)+1);
-        //                 g = Math.round((Math.random()*256)+1);
-        //                 b = Math.round((Math.random()*256)+1);
-        //                 sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(${r}, ${g}, ${b}); `);
-        //             }
-                    
-        //         });
-        //         reset.addEventListener(`click`,() => {
-        //             p.textContent=`${1}`;
-        //             count = p.textContent;
-        //             sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color: rgb(${r}, ${g}, ${b},0.15); `);
-        //         });
-        //     }
-        //     else{
-        //         p.textContent=`${1}`;
-        //         count = p.textContent;
-        //         sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color: rgb(200, 162, 200,10%); `);
-        //         sq.addEventListener(`mouseover`,()=>{
-
-        //         //once counter reaches 10, maximum opacity is achieved
-
-        //             if(p.textContent<=10){                 
-        //                 let count = p.textContent;
-        //                 p.textContent++;
-        //                 sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(200, 162, 200,${(10+(count*10))}%); `);
-        //                 //console.log(count); 
-        //             }
-        //             else{
-        //                 sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color:rgb(200, 162, 200); `);
-        //             }
-                    
-        //         });  
-
-        //         reset.addEventListener(`click`,() => {
-        //             p.textContent=`${1}`;
-        //             count = p.textContent;
-        //             sq.setAttribute(`style`,`flex-basis:${sqs}px; height:${sqs}px; background-color: rgb(200, 162, 200,0.15); `);
-        //         });
-        //     }
-        // });
-
         container.appendChild(sq);
     }
 
@@ -127,44 +58,50 @@ function squares(num){
     return list;
 }
 
-let del = squares(10);
-//console.log(del);
+function remover(mode){
+    
+    function opacityEffect(square){
 
-container.addEventListener(`mouseenter`,bgColor,{
-    capture: true
-});
+        let count =  Number(square.getAttribute(`counter`));
+        if(count<10){
+            square.style.backgroundColor=`rgb(223, 167, 26, ${count/10})`;
+            square.setAttribute(`counter`,`${count+1}`);
+        }
+        else{
+            square.style.backgroundColor=`rgb(223, 167, 26,1)`;
+            square.setAttribute(`counter`,`10`);
+            return;
+        }
+    } 
 
-function bgColor(event){
-    if(rainbowMode){
-        let r = Math.round((Math.random()*256)+1);
-        let g = Math.round((Math.random()*256)+1);
-        let b = Math.round((Math.random()*256)+1);
-        event.target.style.backgroundColor=`rgb(${r},${g},${b},1)`;
+    function bgColor(event){
+        if(event.target !== container){
+            event.stopPropagation();
+
+            if(mode===`Drawing`){
+                event.target.style.opacity = 1;
+                event.target.style.backgroundColor=`rgb(0,0,0,1)`;
+            }
+            else if(mode===`Rainbow`){
+                event.target.style.opacity = 1;
+                let r = Math.round((Math.random()*256)+1);
+                let g = Math.round((Math.random()*256)+1);
+                let b = Math.round((Math.random()*256)+1);
+                event.target.style.backgroundColor=`rgb(${r},${g},${b},1)`;
+            }
+            else if(mode===`Darken`){
+                opacityEffect(event.target);
+            }
+
+        }
+
+        }
+    container.addEventListener(`mouseenter`,bgColor,{
+        capture: true
+    });
+    
     }
-    else{
-        event.target.style.backgroundColor="black";
-    }
-}
 
-size.addEventListener(`click`,() => {
-
-    let input = prompt(`Enter number of squares per side (Max: 60)`);
-    console.log(input);
-
-    del.forEach(element =>{
-        element.remove();
-    })
-
-    let inputNum = Number(input);
-    if(inputNum>0 && inputNum<60){
-        del = squares(inputNum);
-    }
-    else{
-        alert(`Invalid entry`);
-        location.reload();
-    }
-
-});
 
 
 
